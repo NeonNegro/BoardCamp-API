80d193561d669410ba2c10628b5952a14d6a8013
mateuskuritza <mateuskuritza@gmail.com>
2022-03-14 11:29:55 -0300

    Preparando revisão de código

--- src/middlewares/validateSchemaMiddleware.js
+++
@@ -1,9 +0,0 @@
-export default function validateSchemaMiddleware(schema){
-    return (req,res,next) => {
-        const validation = schema.validate(req.body);
-        if(validation.error)
-            return res.status(400).send(validation.error);
-        
-        next();
-    }
-}
